9a160baf403f9696b41c8c49fa3b8c9c961c9141
“zwtghm” <18948223919@163.com>
2023-05-02 15:59:24 +0800

    blog

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function m_1(){
+  $("#l_1").css({"width":"250px"});
+}
+function m_2(){
+  $("#l_1").css({"width":"0"});
+}
+console.log("%czwtghm的博客 📎 CC BY 4.0 .........版本1.0","color:#333;background-color: #bed0fe;border-radius:100px;");
